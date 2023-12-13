e840aca4438c1c6aae27e83702afb9e6298e3849
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-13 17:47:59 -0400

    Add states_list route and import storage from models.state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,6 +2,7 @@
 """Start a Flask web application"""
 from flask import Flask
 from flask import render_template
+from models.state import storage
 app = Flask(__name__)
 
 
@@ -46,15 +47,13 @@ def number_template(n):
 def number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n)
 
-# N
-# ew route
+# New route
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     state_li = storage.all(State).values()
     return render_template('7-states_list.html', states=state_li)
 
 
-# New route
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     storage.close()
