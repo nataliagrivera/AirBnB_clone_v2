28537df9a551d3e13c12ff880d6e0ac0271d0860
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-13 18:17:32 -0400

    Refactor Flask routes and remove unnecessary code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,53 +1,13 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
-from flask import Flask
-from flask import render_template
-from models.state import storage
+'''
+script that starts a Flask web application
+'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)  # route definition
-def hello_hbnb():  # content to display on that route
-    return "Hello HBNB!"
-
-
-# New route
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-# New route
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return "C {}" .format(text.replace("_", " "))
-
-
-# New route
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
-def python(text="is cool"):
-    return "Python {}" .format(text.replace("_", " "))
-
-
-# New route
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return "{} is a number" .format(n)
-
-
-# New route
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
-
-
-# New route
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
-
-# New route
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     state_li = storage.all(State).values()
@@ -60,4 +20,4 @@ def teardown_appcontext(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
