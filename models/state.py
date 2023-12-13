ef9d25adfe2565b7558318d6e8cfa05e2fbeff5b
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-12 23:00:10 -0400

    Refactor State class relationships and fix indentation

--- models/state.py
+++ models/state.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from os import getenv
 import models
 from models.city import City
+from os import getenv
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-
+    __tablename__ = "states"
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete")
-    else:
-        name = ""
+        cities = relationship("City", backref="state", cascade="all, delete")
 
-    @property
-    def cities(self):
-        """Returns the list of City instances with
-        state_id equals to the current State.id"""
-        from models import storage
-        all_cities = storage.all(City)
-        return [city for city in all_cities.values()
-                if city.state_id == self.id]
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """Getter"""
+            city_lst = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_lst.append(city)
+            return city_lst
