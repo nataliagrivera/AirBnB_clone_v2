0c907a276047ab4b54f074c7c4966b3f2e2a34c1
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-14 20:59:33 -0400

    Refactor Flask routes and fix typo in cities_by_states route

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,82 +1,37 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
-from flask import Flask
-from flask import render_template
-from models.state import storage
+"""
+Script that starts a Flask web application.
+"""
+from flask import Flask, render_template
+from models import storage
 from models.state import State
+from models.city import City
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)  # route definition
-def hello_hbnb():  # content to display on that route
-    return "Hello HBNB!"
-
-
-# New route
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-# New route
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return "C {}" .format(text.replace("_", " "))
-
-
-# New route
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python', strict_slashes=False)
-def python(text="is cool"):
-    return "Python {}" .format(text.replace("_", " "))
-
-
-# New route
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return "{} is a number" .format(n)
-
-
-# New route
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
-
-
-# New route
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
-# New route
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
+    """ Route taht displays the html page of
+    a list of states objects sorted by name
+    """
+    states_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_li)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    storage.close()
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    state_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=state_li)
-
-
-@app.route('cities_by_states', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
+    """ Route that display a HTML page with a list of cities
+    objects sorted by name """
     city_li = storage.all(State).values()
-    return render_template('8-cities_by_states.html', cities=city_li)
+    return render_template('8-cities_by_states.html', states=city_li)
 
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
+    """After each request, remove the SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
