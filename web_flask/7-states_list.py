512003465e78264bec6b05d42aa679f072f5182b
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-15 21:51:12 -0400

    Refactor Flask routes and add states_list route

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
-"""
+"""Start a Flask web application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -10,51 +9,58 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def hello():
+    """Display Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Display HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
+def c(text):
+    """Display C followed by text"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
+def python(text='is cool'):
+    """Display Python followed by text"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """Display n is a number only if n is an integer"""
     return '{} is a number'.format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """Display HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
+    """Display HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a HTML page"""
-    states_li = storage.all(State).values()
-    return render_template('7-states_list.html', states=states_li)
+    """Display a HTML page with a list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """After each request, remove the SQLAlchemy Session"""
+def teardown_db(exception):
+    """Close storage"""
     storage.close()
 
 
