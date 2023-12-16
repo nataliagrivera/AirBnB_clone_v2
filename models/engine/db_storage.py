eeb297b5d2f8378e5e846660cdfe8306e58da16c
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-15 22:11:45 -0400

    Refactor DBStorage class and update imports

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,79 +1,73 @@
 #!/usr/bin/python3
-"""
-New engine DBStorage
-"""
+"""New engine DBStorage"""
 
-from models.base_model import BaseModel, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
+from models.base_model import Base
+from models.user import User
 from models.state import State
 from models.city import City
-from models.user import User
 from models.place import Place
-from models.amenity import Amenity
 from models.review import Review
+from models.amenity import Amenity
 
-classes = {'User': User, 'Place': Place,
-           'State': State, 'City': City, 'Amenity': Amenity,
-           'Review': Review}
+classes = {"State": State, "City": City, "User": User,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
-    """" db storage class """
+    """DBStorage class"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Engine constructor"""
-        USER = getenv('HBNB_MYSQL_USER')
-        PWD = getenv('HBNB_MYSQL_PWD')
-        HOST = getenv('HBNB_MYSQL_HOST')
-        DB = getenv('HBNB_MYSQL_DB')
-        # HBNB_ENV = getenv('HBNB_ENV')
-
+        """DBStorage engine constructor"""
+        user = getenv("HBNB_MYSQL_USER")
+        pwd = getenv("HBNB_MYSQL_PWD")
+        host = getenv("HBNB_MYSQL_HOST")
+        db = getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                      .format(USER, PWD, HOST, DB),
+                                      .format(user, pwd, host, db),
                                       pool_pre_ping=True)
-
-        if getenv('HBNB_ENV') == 'test':
+        if getenv('HBNB_ENV') == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Returns query on current database"""
-        a_dict = {}
+        """all method"""
+        new_dict = {}
         if cls is None:
-            for value in classes.values():
-                for o in self.__session.query(value):
-                    k = o.__class__.__name__ + '.' + o.id
-                    a_dict[k] = o
+            for key, value in classes.items():
+                for obj in self.__session.query(value).all():
+                    key = obj.__class__.__name__ + "." + obj.id
+                    new_dict[key] = obj
         else:
-            for o in self.__session.query(classes[cls]):
-                k = o.__class__.__name__ + '.' + o.id
-                a_dict[k] = o
-        return a_dict
+            for obj in self.__session.query(cls).all():
+                key = obj.__class__.__name__ + "." + obj.id
+                new_dict[key] = obj
+        return new_dict
 
     def new(self, obj):
-        """Add object"""
+        """new method"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit changes"""
+        """save method"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete object"""
-        if obj:
+        """delete method"""
+        if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Reloads database"""
+        """reload method"""
         Base.metadata.create_all(self.__engine)
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
-        """Close session"""
+        """close method"""
         self.__session.close()
