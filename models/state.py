876e15c236b6fd81b27ae844b5f46de8e032612e
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-15 21:32:54 -0400

    Refactor State class to improve code readability and maintainability

--- models/state.py
+++ models/state.py
@@ -1,37 +1,21 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy.orm import relationship
-import models
-from models.city import City
 from os import getenv
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from models.city import City
 
 class State(BaseModel, Base):
-    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete")
+
+    else:
         @property
         def cities(self):
-            """Getter for cities related to the current state
-            """
+            """Getter for cities related to the current state"""
             from models import storage
             all_cities = storage.all(City)
-            return [city for city in all_cities.values()
-                    if getattr(city, 'state_id', None) == self.id]
-
-
-    #    @property
-    #    def cities(self):
-    #        """Getter"""
-    #        city_lst = []
-    #        all_cities = models.storage.all(City)
-    #        for city in all_cities.values():
-    #            if city.state_id == self.id:
-    #                city_lst.append(city)
-    #        return city_lst
+            return [city for city in all_cities.values() if getattr(city, 'state_id', None) == self.id]
